30d60e3a74999b7bc3cf60de0a0cad4d4689c5a6
zhangyin <zhangyin@starmerx.com>
2019-06-01 14:15:21 +0800

    re

--- project/RE/RE.py
+++ project/RE/RE.py
@@ -4,8 +4,8 @@
 # Author:张印
 # Desc：
 import re
-def out_re (str,source):
-    print re.match(str, source).group()
+def out_re (str,source,i=0):
+    print re.match(str, source).group(i)
 
 out_re('[0123456789]','2hello pyt2hon')
 out_re('[Hh]','Hhello pyt2hon')
@@ -18,6 +18,34 @@ out_re("[A-Z][a-z]*","MmmadasMdsfsf")
 out_re("[a-zA-Z_]+[\w_]*","_nameas2_")
 out_re("[1-9]?[0-9]","99")
 out_re("[a-zA-Z0-9_]{6}","12a3g45678")
-#匹配出163的邮箱地址，且@符号之前有4到20位，例如hello@163.com
+#匹配出163的邮箱地址，且@符号之前有4到20位，例如h"[1-9]?\d","8"ello@163.com
 out_re("[\w]{4,20}@163\.com$","_Ahello@163.com")
 out_re(r".*\bver\b", "ho ver abc")
+out_re(r".*\Bver\B", "222f13hoverabc")
+out_re("[AD]?\d","D2")
+out_re("[1-9]?\d$","8")
+string = ' 123 dqw  3，213df,adsd'
+print re.sub(' +', ',', string.strip().replace('，', ',')).split(',')
+out_re("[1-9]?\d$|100","90")
+out_re("\w{4,20}@(163|qq|sina)\.com",'asdf123@163.com')
+out_re("([\w+]*)-(\d+)","22-12345678",2)
+out_re(r'<html>\w*</html>','<html>sdf</html>')
+out_re(r'<([a-zA-Z]*)>\w*</\1>','<html>asd122312</html>')
+out_re(r"<(\w*)><(\w*)>.*</\2></\1>", "<html><h1>www.itcast.cn</h1></html>")
+out_re(r"<(?P<name1>\w*)><(?P<name2>\w*)>.*</(?P=name2)></(?P=name1)>", "<html><h1>www.itcast.cn</h1></html>")
+#search 一次性匹配
+print re.search(r"[阅读次数]*(\d+)",'阅读次数sf为9999dff213').group(1)
+#search 全部匹配返回list
+print re.findall(r"\d+", "python = 9999, c = 7890, c++ = 12345")
+#sub 将匹配的数据进行替换
+add = lambda temp:str((int(temp.group())+1))
+print re.sub(r"\d+",add,"python = 996")
+print filter(None,re.split(r"\W","  info:xiaoZhang,33，sh|andong  "))
+
+html2 = '<img data-original="https://rpic.douyucdn.cn/appCovers/2016/11/13/1213973_201611131917_small.jpg" src="https://rpic.douyucdn.cn/appCovers/2016/11/13/1213973_201611131917_small.jpg" style="display: inline;">'
+print re.findall(r'https:.+\.jpg',html2)
+
+url = 'http://www.interoem.com/messageinfo.asp?id=35'
+print re.match(r'http:.+\/',url).group()
+words = 'hello world ha ha'
+print re.split(r' ',words)
--- project/RE/html_re_out.py
+++ project/RE/html_re_out.py
@@ -3,14 +3,30 @@
 # Date: 19-6-1 上午11:07
 # Author:张印
 # Desc：
-import os
-import django
+import re
+html1 = '''<div>
+        <p>岗位职责：</p>
+<p>完成推荐算法、数据统计、接口、后台等服务器端相关工作</p>
+<p><br></p>
+<p>必备要求：</p>
+<p>良好的自我驱动力和职业素养，工作积极主动、结果导向</p>
+<p>&nbsp;<br></p>
+<p>技术要求：</p>
+<p>1、一年以上 Python 开发经验，掌握面向对象分析和设计，了解设计模式</p>
+<p>2、掌握HTTP协议，熟悉MVC、MVVM等概念以及相关WEB开发框架</p>
+<p>3、掌握关系数据库开发设计，掌握 SQL，熟练使用 MySQL/PostgreSQL 中的一种<br></p>
+<p>4、掌握NoSQL、MQ，熟练使用对应技术解决方案</p>
+<p>5、熟悉 Javascript/CSS/HTML5，JQuery、React、Vue.js</p>
+<p>&nbsp;<br></p>
+<p>加分项：</p>
+<p>大数据，数理统计，机器学习，sklearn，高性能，大并发。</p>
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'starpro.settings'
-django.setup()
-from django.core.management.base import BaseCommand
+        </div>'''
+# def replace (html):
+
+print re.sub(r"<.+?>",'',html1).replace('&nbsp;',' ')
+
+html2 = '<img data-original="https://rpic.douyucdn.cn/appCovers/2016/11/13/1213973_201611131917_small.jpg" src="https://rpic.douyucdn.cn/appCovers/2016/11/13/1213973_201611131917_small.jpg" style="display: inline;">'
 
 
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        pass
+print re.match(r"=(.*?)jpg",html2).group(1)
